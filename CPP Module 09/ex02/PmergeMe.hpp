be7aeaf634de4da8dec4755dd90b43b3b865d003
nourddine-benyahya <nourddine.benyahya02@gmail.com>
2025-01-23 19:46:55 +0100

    finish ex02 in c09

--- CPP Module 09/ex02/PmergeMe.hpp
+++ CPP Module 09/ex02/PmergeMe.hpp
@@ -1,21 +1,83 @@
 #include <iostream>
+#include <vector>
+
+
+std::vector<size_t> generateJacobsthal(size_t max_value) {
+    std::vector<size_t> jacobsthal;
+    size_t a = 0, b = 1;
+
+    while (a <= max_value) {
+        if (a != 0)
+            jacobsthal.push_back(a);
+        size_t next = a + 2 * b;
+        a = b;
+        b = next;
+    }
+    return jacobsthal;
+}
+
+template <typename container>
+typename container::iterator binarySearch(container& vec, int target)
+{
+    typename container::iterator low = vec.begin();
+    typename container::iterator high = vec.end();
+
+    high--;
+    while (low < high)
+    {
+        typename container::iterator mid = low + (high - low) / 2;
+        if (*mid == target)
+            return mid;
+        else if (*mid < target)
+            low = mid + 1;
+        else
+            high = mid;
+    }
+    return low;
+}
+
+template <typename container>
+void _sort(container &main, container &tmp)
+{
+    std::vector<size_t> jc = generateJacobsthal(static_cast<size_t>(tmp.size()));
+
+    std::reverse(tmp.begin(),tmp.end());
+    for (std::vector<size_t>::iterator it = jc.begin(); it != jc.end(); ++it)
+    {
+        typename container::iterator elem = binarySearch(main, tmp[(*it) - 1]);
+        main.insert(elem, tmp[(*it) - 1]); 
+    }
+
+    size_t j = 0;
+    for (typename container::iterator it = tmp.begin(); it != tmp.end(); it++)
+    {
+        if (j < jc.size() && *it == tmp[jc[j] - 1])
+        {
+            j++;
+            continue;
+        }
+        typename container::iterator elem = binarySearch(main, *it);
+        main.insert(elem, *it);
+    }
+}
+
 
 template <typename container>
 void sortingalgo(container &main)
 {
-    container other;
-    container tmp;
+    if (main.size() < 3) return;
+
+    container other, tmp;
     size_t i = 0;
-    if (main.size() < 3)
-        return ;
+
     if (main.size() == 3)
     {
         tmp.push_back(main.back());
-        main.erase(main.end());
+        main.pop_back();
     }
     else
     {
-        while (i + 1 < main.size())
+        for (i = 0; i + 1 < main.size(); i += 2)
         {
             if (main[i] > main[i + 1])
             {
@@ -27,24 +89,14 @@ void sortingalgo(container &main)
                 other.push_back(main[i + 1]);
                 tmp.push_back(main[i]);
             }
-            main.erase(main.begin() + i, main.begin() + i + 2);
         }
-        if (main.size() == 1)
-        {
-            other.push_back(main[0]);
-            main.erase(main.begin());
-        }
-        main = other;
+        if (i < main.size())
+            other.push_back(main[i]);
+        main.swap(other); 
     }
-    for (typename container::iterator it = main.begin(); it != main.end(); it++)
-    {
-        std::cout << *it << std::endl;
-    }
-    std::cout << "----------------------" << std::endl;
-    for (typename container::iterator it = tmp.begin(); it != tmp.end(); it++)
-    {
-        std::cout << *it << std::endl;
-    }
-    std::cout << "+++++++++++++++++++++" << std::endl;
+
     sortingalgo(main);
+    if (main.size() == 2 && main[0] > main[1])
+        std::swap(main[0], main[1]);
+    _sort(main, tmp);
 }
--- CPP Module 09/ex02/main.cpp
+++ CPP Module 09/ex02/main.cpp
@@ -1,17 +1,64 @@
 #include "PmergeMe.hpp"
 #include <vector>
+#include <deque>
+#include <iomanip>
 
-int main()
+bool nonAnumber(const std::string data)
+{
+    size_t len = data.length();
+    size_t i = 0;
+    if (data[0] == '+' || data[0] == '-')
+        i++;
+    std::string number("0123456789");
+    while (i < len && number.find(data[i++]) != std::string::npos);
+    return i != len || data[0] == '.' ||  (i == len && number.find(data.at(len - 1)) == std::string::npos) || (data.find("f") != data.rfind("f")) ? true : false;
+}
+
+int main(int ac, char *av[])
 {
     std::vector<int> vec;
-    vec.push_back(6);
-    vec.push_back(17);
-    vec.push_back(31);
-    vec.push_back(57);
-    vec.push_back(46);
-    vec.push_back(12);
-    vec.push_back(35);
-    vec.push_back(4);
-    vec.push_back(5);
+    std::deque<int> deq;
+    if (ac <= 2)
+    {
+        std::cout << "u should enter more than one number" << std::endl;
+        return 1;
+    }
+    for (int i = 1; i < ac; i++)
+    {
+        if (nonAnumber(av[i]))
+        {
+            std::cerr << "invalid data" << std::endl; 
+            return 1;
+        }
+        vec.push_back(atoi(av[i]));
+        deq.push_back(atoi(av[i]));
+    }
+
+    std::cout << "Before: ";
+    for (std::vector<int>::iterator itt = vec.begin(); itt != vec.end(); itt++)
+    {
+        std::cout << " " << *itt;
+    }
+    std::cout << std::endl;
+
+    clock_t start_time = clock();
     sortingalgo(vec);
+    clock_t end_time = clock();
+    double elapsed_time_us = static_cast<double>(static_cast<double>(end_time) - static_cast<double>(start_time)) / CLOCKS_PER_SEC * 10;
+
+    clock_t start_time1 = clock();
+    sortingalgo(deq);
+    clock_t end_time1 = clock();
+    double elapsed_time_us1 = static_cast<double>(static_cast<double>(end_time1) - static_cast<double>(start_time1)) / CLOCKS_PER_SEC * 10;
+
+    std::cout << "After: ";
+    for (std::vector<int>::iterator itt = vec.begin(); itt != vec.end(); itt++)
+    {
+        std::cout << " " << *itt;
+    }
+    std::cout << std::endl;
+
+    std::cout << "Time to process a range of "<< vec.size() <<" elements with std::vector : " << elapsed_time_us << " us" << std::endl;
+    std::cout << "Time to process a range of "<< deq.size() <<" elements with std::deque : "<< elapsed_time_us1 << " us" << std::endl;
+
 }
